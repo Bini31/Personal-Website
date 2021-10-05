6790bb247735b7b1dfbe8a4e87d9abc10b75c0dc
shajeev kumar <k.b.binitha@gmail.com>
2021-10-05 13:30:38 +0400

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+export const Navbar = () => {
+    return (
+        <div>
+           <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+ <div className="container">
+
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <FontAwesomeIcon icon={faBars} style={{color:'#fff'}}/>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link text-white" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="#">About Us</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="#">Experience</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="#">Contact Me</a>
+      </li>
+     
+     
+    </ul>
+   
+  </div></div>
+</nav> 
+        </div>
+    )
+}
